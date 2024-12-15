a909ecefd785c6f6ac47f2debfb40cc5c806129c
admin <allammaneesha98@gmail.com>
2024-12-15 17:15:27 +0530

    commiting practice1

---
+++ Maneeshapracticeuser/Source/Newpackage/Newpackage.java
@@ -0,0 +1,12 @@
+package Newpackage;
+
+public class Newpackage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("The new package is :");
+
+	}
+
+}
